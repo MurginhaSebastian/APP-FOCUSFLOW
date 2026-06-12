474e3b6df0c297482aabf1da9e4b876c4370785c
Angelo <angelo10191817@gmail.com>
2026-06-11 21:21:26 -0500

    Configuracion final para subir v1.0 del proyecto

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.FocusFlow"
+        applicationId = "com.neocode.FocusFlow"
         minSdk = 24
         targetSdk = 35
         versionCode = 1
--- app/src/main/java/com/example/focusflow/data/remote/ApiConfig.kt
+++ app/src/main/java/com/example/focusflow/data/remote/ApiConfig.kt
@@ -2,5 +2,5 @@ package com.example.focusflow.data.remote
 
 object ApiConfig {
     const val WEATHER_API_KEY = "ee8f51332b8afff947dbb91fd5d3ccda"
-    const val GROQ_API_KEY = "" // Pon tu clave localmente. No la subas a GitHub.
+    const val GROQ_API_KEY = ""
 }
